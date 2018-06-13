7dfb5288d2d0173fad8271361ffb9fd38e048f5e
Pedro Girardi <pedro.rogerio.girardi@gmail.com>
2018-06-13 12:21:52 +0700

    Initial Light Code implementation
    - Rename bin;
    - Log to /tmp/lightcode.out;
    - LightCode extension interface;
    - REPL ns;

---
+++ src/lightcode/server/LightCodeExtension.java
@@ -0,0 +1,10 @@
+package lightcode.server;
+
+import org.eclipse.lsp4j.jsonrpc.services.JsonRequest;
+
+import java.util.concurrent.CompletableFuture;
+
+public interface LightCodeExtension {
+    @JsonRequest
+    CompletableFuture<String> repl(String message);
+}
